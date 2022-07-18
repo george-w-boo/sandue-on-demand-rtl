a3ce3736955700f60716b40fa083310762ae3ec8
Mykhailo Ostapysko <mykhailo.ostapysko@gmail.com>
2022-07-18 15:27:29 +0300

    adds SummaryForm component content

--- test-sundaes-on-demand/src/pages/Summary/SummaryForm.jsx
+++ test-sundaes-on-demand/src/pages/Summary/SummaryForm.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const SummaryForm = () => {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{color: 'blue'}}>Terms and Conditions</span>
+    </span>
+  )
   return (
-    <div>SummaryForm</div>
+    <Form>
+      <Form.Group controlId='terms-and-conditions'>
+        <Form.Check
+          type='checkbox'
+          checked={tcChecked}
+          onChange={e => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button
+        variant='primary'
+        type='submit'
+        disabled={!tcChecked}
+      >
+        Confirm order
+      </Button>
+    </Form>
   )
 }
 
