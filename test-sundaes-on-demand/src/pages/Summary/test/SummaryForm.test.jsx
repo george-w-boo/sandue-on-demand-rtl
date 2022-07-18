d419ccb20322e9cc9364217403cec06afc16324e
Mykhailo Ostapysko <mykhailo.ostapysko@gmail.com>
2022-07-18 16:05:59 +0300

    adds tests for popover in SummaryForm

--- test-sundaes-on-demand/src/pages/Summary/test/SummaryForm.test.jsx
+++ test-sundaes-on-demand/src/pages/Summary/test/SummaryForm.test.jsx
@@ -1,19 +1,40 @@
 import SummaryForm from "../SummaryForm";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('SummaryForm', () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i});
-  const button = screen.getByRole('button', {name: /confirm order/i});
-
   test('checkbox functionality', () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i});
+    const button = screen.getByRole('button', {name: /confirm order/i});
+
     expect(checkbox).not.toBeChecked();
     expect(button).toBeDisabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(button).toBeDisabled();
+  })
+
+  test('popover responds to hover', () => {
+    render(<SummaryForm />);
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i)
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+    userEvent.hover(termsAndConditions);
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when mouse out
+    userEvent.unhover(termsAndConditions);
+    const nullPopoverAgain = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(nullPopoverAgain).not.toBeInTheDocument();
   })
 })
