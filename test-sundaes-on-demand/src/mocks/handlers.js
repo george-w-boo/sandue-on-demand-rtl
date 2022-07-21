1b6783e6fadff0a23f86196be640ec0883c8e012
Mykhailo Ostapysko <mykhailo.ostapysko@gmail.com>
2022-07-21 11:46:13 +0300

    adds new mock handler for postin and order

--- test-sundaes-on-demand/src/mocks/handlers.js
+++ test-sundaes-on-demand/src/mocks/handlers.js
@@ -19,4 +19,12 @@ export const handlers = [
       ])
     );
   }),
+  rest.post("http://localhost:3030/order", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+      ])
+    );
+  }),
 ];
